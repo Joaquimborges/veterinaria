1862bf06be168ec78602b07f826d93f9a8d523c8
mark-oliveira-ml <89139884+mark-oliveira-ml@users.noreply.github.com>
2021-09-23 20:00:28 -0300

    Adicionada a função Atualização (Uptate) do médico, que recebe já um médico editado e validado. fazemos um swap do novo com o velho, usando delete() e cadastrar() que já lidam com o json.
    Escolhi retornar o medico desatualizado caso façamos ou retornemos algo de informação sobre a deleção. MAS... é algo que pode ser discutido (e deve).
    
    ApplicationEnums agora possui o enum camposEditaveis que nos auxiliarão a passar nas outras camadas os campos nos quais queiramos modificar nas entidades.
    
    ---`;-,-{@
    
    Peço a gentileza de

---
+++ src/main/java/com/veterinaria/entity/ApplicationEnums.java
@@ -0,0 +1,31 @@
+package com.veterinaria.entity;
+
+public class ApplicationEnums {
+
+
+        enum especialidadeMedico
+        {
+            ANIMAL_DOMESTICO,
+            ANIMAL_FAZENDA,
+            ANIMAL_DOMESTICO_FAZENDA,
+            ANIMAL_SELVAGEM,
+            TODOS
+        }
+
+        enum tipoAnimal
+        {
+            DOMESTICO,
+            FAZENDA,
+            SELVAGEM
+        }
+
+        enum camposEditaveis
+        {
+            ESPECIALIDADE,
+            NOME,
+            RACA,
+            SEXO,
+            RG
+        }
+
+}
