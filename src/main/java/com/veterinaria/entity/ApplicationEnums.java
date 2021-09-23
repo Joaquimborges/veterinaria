c7cd170a6eb6a744e2b99944aebb37b6edb06130
mark-oliveira-ml <89139884+mark-oliveira-ml@users.noreply.github.com>
2021-09-23 11:05:49 -0300

    "Adicionada a classe 'AppEnums' que contem enumeradores das constantes convenientes à nossa aplicação. Neste commit:
    * especialidadeMedico
    * tipoAnimal

---
+++ src/main/java/com/veterinaria/entity/ApplicationEnums.java
@@ -0,0 +1,20 @@
+package com.veterinaria.entity;
+
+public class ApplicationEnums {
+
+    enum especialidadeMedico
+    {
+        ANIMAL_DOMESTICO,
+        ANIMAL_FAZENDA,
+        ANIMAL_DOMESTICO_FAZENDA,
+        ANIMAL_SELVAGEM,
+        TUTTI
+    }
+
+    enum tipoAnimal
+    {
+        DOMESTICO,
+        FAZENDA,
+        SELVAGEM
+    }
+}
