de12888cabe701e8c4d2c85425bfb7455f270183
joaquimborges <joaquim.borges1993@gmail.com>
2021-09-24 11:58:24 -0300

    implementado os seguintes metodos na classe ProprietarioService:
    cadastrar sem duplicitadade.
    obter um.
    alterar e listar.

--- src/main/java/com/veterinaria/service/ProprietarioService.java
+++ src/main/java/com/veterinaria/service/ProprietarioService.java
@@ -3,7 +3,11 @@ package com.veterinaria.service;
 
 import com.veterinaria.entity.Proprietario;
 import com.veterinaria.persistence.ProprietarioPersistence;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class ProprietarioService {
 
     private final ProprietarioPersistence persistence = new ProprietarioPersistence();
@@ -16,5 +20,52 @@ public class ProprietarioService {
         }
         return true;
     }
+
+
+    public Proprietario cadastrar(Proprietario proprietario){
+        if (cpfNaoUtilizado(proprietario.getCpf())){
+            try {
+                persistence.cadastrar(proprietario);
+                return proprietario;
+            }catch (RuntimeException e){
+                e.printStackTrace();
+            }
+        }else {
+            throw new RuntimeException("Verifica as informações, CPF em uso!");
+        }
+        return null;
+    }
+
+
+    public Proprietario getProprietario(String cpf){
+        return persistence.obterUm(cpf);
+    }
+
+
+    public List<Proprietario> Listar(){
+        return persistence.listarProprietarios();
+    }
+
+
+    public Proprietario altera(Proprietario proprietario){
+        if (proprietario != null){
+            return persistence.altera(proprietario);
+        }
+        return null;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
