3223548659b7c0fc3670403a66f19603e6399445
mark-oliveira-ml <89139884+mark-oliveira-ml@users.noreply.github.com>
2021-09-25 10:09:19 -0300

    Adicionada MedicoPersistem. branch problemática excluída.

---
+++ src/main/java/com/veterinaria/service/PacienteService.java
@@ -0,0 +1,67 @@
+package com.veterinaria.service;
+
+import com.veterinaria.entity.Consulta;
+import com.veterinaria.entity.Paciente;
+import com.veterinaria.entity.Proprietario;
+import com.veterinaria.persistence.ConsultaPersistence;
+import com.veterinaria.persistence.PacientePersistence;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+import java.util.Optional;
+
+public class PacienteService
+{
+
+    @Autowired
+    PacientePersistence pacientePersistence;
+    ConsultaPersistence consultaPersistence;
+
+    public Paciente cadastraPaciente (Paciente paciente)
+    {
+
+        if(paciente.getProprietario() != null)
+        {
+
+            pacientePersistence.cadastrar(paciente);
+            return paciente;
+        }
+
+        return null;
+    }
+
+    public Paciente obterPaciente(String nome, String proprietarioCpf) {
+
+        return pacientePersistence.obterPaciente(nome, proprietarioCpf);
+
+    }
+
+    public Paciente altera(Paciente paciente){
+        if (paciente.getProprietario() != null)
+        {
+            pacientePersistence.altera(paciente);
+        }
+        return null;
+    }
+
+    private boolean pacienteNaoExisteNaConsulta(String cpf){
+
+
+        for (Consulta consulta : consultaPersistence.listar()){
+            if (consulta.getPaciente().getProprietario().getCpf().equals(cpf)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean apagar(String nome, String cpf)
+    {
+        if (pacienteNaoExisteNaConsulta(cpf))
+            return pacientePersistence.remove(nome, cpf);
+        return false;
+    }
+
+
+
+}
