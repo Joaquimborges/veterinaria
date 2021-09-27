6c64406bb4f9ddc06a950d94fdeda7dc0fcfee2b
mark-oliveira-ml <89139884+mark-oliveira-ml@users.noreply.github.com>
2021-09-27 11:06:01 -0300

    Atualizando MedicoService. Adicionado Medico Controller, medicoDTO(response / request) e um validador que deve ser discutido onde deve ser posto.

--- src/main/java/com/veterinaria/dto/medico/MedicoDtoRequest.java
+++ src/main/java/com/veterinaria/dto/medico/MedicoDtoRequest.java
@@ -1,31 +1,75 @@
 package com.veterinaria.dto.medico;
 
+import com.veterinaria.controller.Validador;
 import com.veterinaria.entity.Medico;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@Setter
+import javax.xml.validation.Validator;
+
 @Getter
-public class MedicoDtoRequest {
+@Setter
+public class MedicoDTORequest
+{
+    /*
+        Criação da classe MedicoRequest
+
+            -> Está classe deve ser criado na package dto.
+            -- Atributos
+            -cpf: String
+            -nome: String
+            -sobrenome: String
+            -especialidade: String
+            -numeroRegistro: String -> Integer, certo?
+
+            Criar método toString() utilizando a sobreescrita @Override
+
+            Estas classes devem conter os atributos que forem escolhidos para expor no request.
+            Deve conter tb os métodos de conversão de DTO para objetoClasse
+     */
 
     private String nome;
     private String sobrenome;
     private String cpf;
-    private Integer numeroRegistro;
+    private Integer crvet;
     private String especialidade;
 
-    public MedicoDtoRequest() {
-    }
+    @Autowired
+    Validador validator;
 
-    public MedicoDtoRequest(String nome, String sobrenome, String cpf, Integer numeroRegistro, String especialidade) {
+    public MedicoDTORequest(){}
+
+    public MedicoDTORequest(String nome, String sobrenome, String cpf, Integer crvet, String especialidade) {
         this.nome = nome;
         this.sobrenome = sobrenome;
         this.cpf = cpf;
-        this.numeroRegistro = numeroRegistro;
+        this.crvet = crvet;
         this.especialidade = especialidade;
     }
 
-    public Medico converte(){
-        return new Medico(nome, sobrenome, cpf, numeroRegistro, especialidade);
+    public Medico converte (MedicoDTORequest medico)
+    {
+        boolean valida = validator.cpfValido(medico.cpf) && validator.validaEspecialidade(medico.especialidade) && validator.validaEspecialidade(medico.especialidade);
+        if (valida) return new Medico(nome, sobrenome, cpf, crvet, especialidade);
+        return null;
+    }
+
+
+
+
+
+
+
+
+    @Override
+    public String toString() {
+        return "{\"MedicoRequest\":{"
+                + "                        \"nome\":\"" + nome + "\""
+                + ",                         \"sobrenome\":\"" + sobrenome + "\""
+                + ",                         \"cpf\":\"" + cpf + "\""
+                + ",                         \"crvet\":\"" + crvet + "\""
+                + ",                         \"especialidade\":\"" + especialidade + "\""
+                + "}}";
     }
 }
