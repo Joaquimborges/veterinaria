f7e84254c6f45095013bd32cfbefbea6d4f7c86b
mark-oliveira-ml <89139884+mark-oliveira-ml@users.noreply.github.com>
2021-10-04 19:45:36 -0300

    Métodos: cadastraPaciente() e alteraPaciente()

--- src/test/java/com/veterinaria/PacienteServiceTest.java
+++ src/test/java/com/veterinaria/PacienteServiceTest.java
@@ -22,7 +22,8 @@ public class PacienteServiceTest {
     ConsultaPersistence mockConsultaPersistence = Mockito.mock(ConsultaPersistence.class);
     ProprietarioPersistence mockProprietarioPersistence = Mockito.mock(ProprietarioPersistence.class);
 
-    ArrayList<Paciente> lista = new ArrayList<>();
+    ArrayList<Paciente> listaPaciente = new ArrayList<>();
+    ArrayList<Proprietario> listaProprietario = new ArrayList<>();
     ArrayList<Consulta> listaConsulta = new ArrayList<>();
 
     LocalDate data = LocalDate.of(2021, 9, 25);
@@ -31,76 +32,52 @@ public class PacienteServiceTest {
     Proprietario proprietario2 = new Proprietario("Mauro", "Rocha", "11773344556", "Rua 2", "91276338902", data);
 
     Paciente paciente1 = new Paciente("cachorro", "branca", "bulldog", "Amora", "femea", data, proprietario);
+    //Paciente paciente1update = new Paciente("cachorro", "branca-e-preto", "bulldog", "Amora", "femea", data, proprietario);
+
     Paciente paciente2 = new Paciente("cachorro", "preto", "bulldog", "Apollo", "macho", data, proprietario2);
+
+    /**
+     * Autor: Marcelo de Oliveira Santos
+     * Data: 04.10.2021
+     */
 
     @Test
     void cadastrarPacienteTest(){
         Mockito.when(mockPacientePersistence.cadastrar(Mockito.any(Paciente.class))).thenReturn(paciente1);
-        lista.add(paciente1);
-        Mockito.when(mockPacientePersistence.listarPacientes()).thenReturn(lista);
+        listaProprietario.add(proprietario);
 
-        PacienteService pacienteService = new PacienteService(mockPacientePersistence);
+        Mockito.when(mockProprietarioPersistence.listarProprietarios()).thenReturn(listaProprietario);
 
-        pacienteService.cadastraPaciente(paciente1);
+        PacienteService pacienteService = new PacienteService(mockPacientePersistence, mockProprietarioPersistence, mockConsultaPersistence);
 
-//        assertNotNull(paciente1);
-        assertTrue(lista.contains(paciente1));
+        Paciente p = pacienteService.cadastraPaciente(paciente1);
+
+        assertNotNull(p);
+        assertTrue(p.getNome().equals(paciente1.getNome()));
+
     }
 
-//    @Test
-//    void pacienteEmUsoTest(){
-//        Mockito.when(mockPacientePersistence.cadastrar(Mockito.any(Paciente.class))).thenReturn(paciente1);
-//        lista.add(paciente1);
-//        Mockito.when(mockPacientePersistence.listarPacientes()).thenReturn(lista);
-//        PacienteService pacienteService = new PacienteService();
-//
-//        pacienteService.cadastraPaciente(paciente1);
-//
-//        assertTrue(paciente1.getNome().equals("Mauro"));
-//    }
-
-
+    /**
+     * Autor: Marcelo de Oliveira Santos
+     * Data: 04.10.2021
+     */
     @Test
     void alteraPaciente(){
-        lista.add(paciente1);
+        listaPaciente.add(paciente1);
 
         Mockito.when(mockPacientePersistence.altera(Mockito.any(Paciente.class))).thenReturn(paciente1);
-        Mockito.when(mockPacientePersistence.listarPacientes()).thenReturn(lista);
+        Mockito.when(mockPacientePersistence.listarPacientes()).thenReturn(listaPaciente);
 
         PacienteService pacienteService = new PacienteService(mockPacientePersistence);
 
         pacienteService.altera(paciente1);
 
-        String expectedNome = "Rafael";
+        String expectedNome = "Amarildo";
         String actualNome   = paciente1.getNome();
-    }
 
-    @Test
-    void listaPaciente(){
-        Mockito.when(mockPacientePersistence.remove(Mockito.any(String.class), Mockito.any(String.class))).thenReturn(true);
-        Mockito.when(mockPacientePersistence.listarPacientes()).thenReturn(lista);
-
-        PacienteService pacienteService = new PacienteService(mockPacientePersistence);
-
-        pacienteService.listarPacientes();
-
-        assertNotNull(paciente1);
+        assertEquals(expectedNome, actualNome);
     }
 
 
-    @Test
-    void apagarPaciente(){
-
-        Mockito.when(mockPacientePersistence.remove(Mockito.any(String.class), Mockito.any(String.class))).thenReturn(true);
-        Mockito.when(mockConsultaPersistence.listar()).thenReturn(listaConsulta);
-        Mockito.when(mockPacientePersistence.listarPacientes()).thenReturn(lista);
-
-        PacienteService pacienteService = new PacienteService(mockPacientePersistence,mockProprietarioPersistence,mockConsultaPersistence);
-
-        pacienteService.apagar(paciente1.getNome(), proprietario.getCpf());
-
-        assertNotEquals(true, lista.contains(paciente1));
-
-    }
 
 }
